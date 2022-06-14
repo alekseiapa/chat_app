d388c2398262c6dcd9b58c1b7bee96d4369e834b
alekseiapa <aapaseev@bk.ru>
2022-06-14 23:24:58 +0800

    feat: Added the init boilerplate for the chat

--- chat_app/src/App.js
+++ chat_app/src/App.js
@@ -1,7 +1,24 @@
+import { ChatEngine } from "react-chat-engine";
+import ChatFeed from "./components/ChatFeed";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <ChatEngine
+        height="100vh"
+        projectID={process.env.REACT_APP_PROJECTID}
+        userName={process.env.REACT_APP_USERNAME}
+        userSecret={process.env.REACT_APP_PASSWORD}
+        renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+        onNewMessage={() =>
+          new Audio(
+            "https://chat-engine-assets.s3.amazonaws.com/click.mp3"
+          ).play()
+        }
+      ></ChatEngine>
+    </div>
+  );
 }
 
 export default App;
---
+++ chat_app/src/components/HisMessage.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HisMessage() {
+  return <div>HisMessage</div>;
+}
