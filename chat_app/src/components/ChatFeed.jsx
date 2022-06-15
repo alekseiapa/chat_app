1ba3b4bde6d95ca3e9aec05a3f78aac6d54eaeb3
alekseiapa <aapaseev@bk.ru>
2022-06-15 23:10:54 +0800

    feat: add chat feed

--- chat_app/src/components/ChatFeed.jsx
+++ chat_app/src/components/ChatFeed.jsx
@@ -1,5 +1,18 @@
 import React from "react";
-
-export default function ChatFeed() {
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import HisMessage from "./HisMessage";
+export default function ChatFeed(props) {
+  const { chats, activeChat, userName, messages } = props;
+  const chat = chats && chats[activeChat];
+  const renderMEssages = () => {
+    const keys = Object.keys(messages);
+    return keys.map((key, index) => {
+      const message = messages[key];
+      const lastMessageKey = index === 0 ? null : keys[index - 1];
+      const isMyMessage = (userName = userName === message.sender.userName);
+      return <div key={`msg_${index}`}></div>;
+    });
+  };
   return <div>ChatFeed</div>;
 }
